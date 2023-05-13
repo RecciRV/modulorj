9b7b80f0b8d480ea9fc765206f94d8e3df8103af
rene <renejuarezh@hotmail.com>
2023-05-12 19:14:10 -0600

    primer commit

---
+++ manifest.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'modulorj',
+    'summary': 'Breve descripción de tu módulo',
+    'description': 'Descripción más detallada de tu módulo',
+    'author': 'Tu nombre',
+    'website': 'Tu sitio web',
+    'version': '1.0',
+    'category': 'Categoría',
+    'depends': ['base'],
+    'data': [
+        'templates/template.html',
+        'security/security.xml',
+        'views/view.xml',
+        'data/data.xml',
+         'index.html',
+    'static/assets/css/style.css',
+    'static/assets/js/script.js',
+    'static/assets/img/image.png',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
